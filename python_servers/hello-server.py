66a91a6f3b4e5268037efdcd06ac863bc5367383
Christopher Ashton <ashtoncr@gmail.com>
2018-06-09 21:55:45 -0400

    basic python servers complete

---
+++ python_servers/hello-server.py
@@ -0,0 +1,30 @@
+'''
+Starter code fetched from
+https://github.com/udacity/course-ud303/tree/master/Lesson-2/0_HelloServer/HelloServer.py
+on 7 June 2018 as part of the Udacity.com Full-Stack-Nanodegree coursework
+'''
+#!/usr/bin/env python3
+#
+# The *hello server* is an HTTP server that responds to a GET request by
+# sending back a friendly greeting.  Run this program in your terminal and
+# access the server at http://localhost:8000 in your browser.
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+
+        # Now, write the response body.
+        self.wfile.write("Hello, HTTP!\n".encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)  # Serve on all addresses, port 8000.
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
